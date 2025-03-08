335b9b9a38a059815a02be4be276a4bd4173bea9
4JN22CS091 <muthahira04@gmail.com>
2025-03-08 19:42:09 +0530

    final commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").createServer(app);
+const {Server} = require("socket.io");
+
+const io = new Server(server);
+//routes
+app.get("/",(req,res)=>{
+  res.send("This is mern realtime board sharing app official server ");
+});
+
+io.on("connection",(socket)=>{
+  console.log("User connected");
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port,()=>console.log("server is running on http://localhost:5000"))
